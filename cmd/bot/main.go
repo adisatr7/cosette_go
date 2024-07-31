71201d908ebc83599e372e49eacbf96a1e9e1982
AdiSatR7 <adisatr7@gmail.com>
2024-07-31 20:03:47 +0700

    Bot can now go online

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,9 +2,53 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file!")
+	}
+
+	token := os.Getenv("TOKEN")
+	if token == "" {
+		log.Fatal("No token found in .env file!")
+	}
+
+	bot, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal("Error creating Discord session: ", err)
+	}
+
+	bot.AddHandler(func(session *discordgo.Session, message *discordgo.MessageCreate) {
+		if message.Author.ID == session.State.User.ID {
+			return
+		}
+
+		if message.Content == "ping" {
+			session.ChannelMessageSend(message.ChannelID, "pong")
+		}
+	})
+
+	bot.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = bot.Open()
+	if err != nil {
+		log.Fatal("Error opening Discord session: ", err)
+	}
+	defer bot.Close()
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
